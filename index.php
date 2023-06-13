ed8a91007c747397514061f9ba3297d762247b27
Costiuc Igor <90152687+Xenilae@users.noreply.github.com>
2023-06-13 23:29:56 +0300

    working with syntax

--- index.php
+++ index.php
@@ -1,7 +1,22 @@
 <?php
+include "data_types.php";
 
-    echo "Eu o sa îmi cumpăr un sport car peste un an!";
-    echo "<br>";
-    echo date(DATE_RSS);
+echo "<br>";
+echo "Eu o sa îmi cumpăr un sport car peste un an!";
+echo "<br>";
+echo date(DATE_RSS);
+
+echo "<br>";
+echo "<br>";
+echo "Additional functionality:";
+echo "<br>";
+
+$targetDate = strtotime("+1 year");
+$currentDate = time();
+$secondsDiff = $targetDate - $currentDate;
+$daysDiff = floor($secondsDiff / (60 * 60 * 24));
+
+echo "Days until I buy a sports car: " . $daysDiff . " days";
+?>
 
 
