d54a6fd1d0b417ac66f4842d3b68ab994e9334e8
Costiuc Igor <90152687+Xenilae@users.noreply.github.com>
2023-06-17 22:46:09 +0300

    funny datatypes

--- data_types.php
+++ data_types.php
@@ -1,3 +1,25 @@
 <?php
 
-echo "test";
+// Integer
+$number = 10;
+
+// Floating point number
+$float = 3.14;
+
+// Boolean
+$flag = true;
+
+// String
+$text = "test";
+
+// Array
+$array = [1, 2, 3, 4, 5];
+
+// Object
+$object = new stdClass();
+
+// Null
+$nullValue = null;
+
+echo $text;
+
